550ba71a41f5723f4bb70fbcaa496f5a70aaacad
Gurpreet <Gurpreet@DESKTOP-HFBPOCJ.wireless.concordia.ca>
2016-02-02 22:52:51 -0500

    Corrected  calculation of Mouse click with dyanmic grid size.

--- GameProject/src/main/Player.java
+++ GameProject/src/main/Player.java
@@ -25,7 +25,8 @@ public class Player {
 	public void setTile(){
 		//TODO do not set tile multiple times i.e. set the tile only once and add toggle effect
 		//TODO set only valid tile
-		if((Mouse.getX() / blockSize) < Boot.getNoRows())
+		
+		if(((Mouse.getX() / blockSize) < Boot.getNoColumns()) && (((HEIGHT - Mouse.getY()) / blockSize) < Boot.getNoRows()))
 			//&& (HEIGHT-Mouse.getY()-1) <=Boot.getNoColumns())
 			if(Mouse.isButtonDown(0)) // if left mouse key is pressed
 				grid.setTile((int)Math.floor(Mouse.getX() / blockSize),(int)Math.floor((HEIGHT-Mouse.getY()-1) / blockSize),TileType.Dirt);
